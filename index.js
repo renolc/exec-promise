58cde7ac6fcabc10cd21eb2ed90cd98f07f05d57
Phillip Gibson <gibson.phillip.j@gmail.com>
2017-02-25 10:34:51 -0500

    Return the promise. doh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec
 
 module.exports = function (cmd) {
-  new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
     exec(cmd, function (err, stdout, stderr) {
       if (err) return reject(err)
       resolve(stdout || stderr)
